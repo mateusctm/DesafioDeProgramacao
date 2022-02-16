53a1fe88c2691a8800e51d6dd303f5dda2a0e130
mateus <mateusctm@gmail.com>
2022-02-16 18:55:05 -0300

    adicionando metodo escada primeira questão de programação

--- desafioDeProgramacao/src/com/company/Main.java
+++ desafioDeProgramacao/src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
 public class Main {
+    public static void escada(int n){
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+
+
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        escada(6);
     }
 }
-    
